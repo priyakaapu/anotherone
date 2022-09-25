bcc12876cc22bbf38f61c95e3d8d1e566ff2ce16
priyakaapu <priya.kaapu@gmail.com>
2022-09-25 21:36:28 +0530

    commit new changes

---
+++ TDD/NewTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class NewTest {
+  
+  @Test
+  public void tC1() {
+     System.out.println("Test Case 1");
+  }
+  
+}
